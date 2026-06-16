56bb7cb5801a1e31189d21152b92ff53846e4ea4
Yu Min Choi <yuminchoi@yuminchoi.local>
2026-06-16 16:44:02 +0900

    netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/pawspective/",
   plugins: [svelte()],
+  base: "./",
 });
